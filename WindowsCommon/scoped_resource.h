8c9156c716fb1297ca6a4e3782d8768d9324c6c5
Toby Jones <ace_roqs@yahoo.com>
2014-03-23 23:33:47 -0700

    More complete implementation of N3830.

--- WindowsCommon/scoped_resource.h
+++ WindowsCommon/scoped_resource.h
@@ -1,11 +1,11 @@
-#ifndef STD_OPT_SCOPED_RESOURCE_H
-#define STD_OPT_SCOPED_RESOURCE_H
-
-// scoped_resource.h : Implementation of N3830 - Scoped Resource - Generic RAII Wrapper for the Standard Library.
-// N3830 is a proposal for a generic RAII wrapper.
-// This is a partial implementation for VC2010, which doesn't include variadic template support.
+// N3830 - Scoped Resource - Generic RAII Wrapper for the Standard Library.
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2014/n3830.pdf
+// This is a partial implementation of N3830, implemented for Visual C++ 2010, which is not C++14 ready.
 // Implemented by Toby Jones.
 // TODO: This is not Windows specific.  It should be in a more general library than WindowsCommon.
+
+#ifndef STD_OPT_SCOPED_RESOURCE_H
+#define STD_OPT_SCOPED_RESOURCE_H
 
 #include <utility>
 
@@ -13,40 +13,93 @@ namespace std_opt
 {
 
 #define NOEXCEPT
+#define EQUALS_DELETE
+
+enum invoke_it
+{
+    once,
+    again
+};
 
 template <typename DELETER, typename RES>
 class scoped_resource
 {
     DELETER m_deleter;
     RES m_resource;
-    bool m_should_run;
+    bool m_execute_on_destruction;
+
+    scoped_resource& operator=(scoped_resource const&) EQUALS_DELETE;
+    scoped_resource(scoped_resource const&) EQUALS_DELETE;
 
 public:
-    //explicit scoped_resource(DELETER&& deleter, R&&... resource, bool shouldRun = true) noexcept;
-    explicit scoped_resource(DELETER&& deleter, RES&& resource, bool should_run = true) NOEXCEPT : m_deleter(std::move(deleter)), m_resource(std::move(resource)), m_should_run(should_run)
+    explicit scoped_resource(DELETER&& deleter, RES&& resource, bool should_run = true) NOEXCEPT : m_deleter(std::move(deleter)), m_resource(std::move(resource)), m_execute_on_destruction(should_run)
     {
     }
 
-    //explicit scoped_resource(const DELETER& deleter, const R&... resource, bool shouldRun = true) noexcept;
-    //scoped_resource(scoped_resource &&other) noexcept;
-    //scoped_resource& operator=(scoped_resource &&other) noexcept;
+    explicit scoped_resource(const DELETER& deleter, const RES& resource, bool should_run = true) NOEXCEPT : m_deleter(deleter), m_resource(resource), m_execute_on_destruction(should_run)
+    {
+    }
+
+
+    scoped_resource(scoped_resource&& other) NOEXCEPT : m_deleter(other.m_deleter), m_resource(std::move(other.m_resource)), m_execute_on_destruction(other.m_execute_on_destruction)
+    {
+        other.m_execute_on_destruction = false;
+    }
+
+    scoped_resource& operator=(scoped_resource&& other) NOEXCEPT
+    {
+        // TODO: If m_resource was movable, what is the point of scoped_resource?
+        invoke();
+        m_resource = std::move(other.m_resource);
+        other.m_execute_on_destruction = false;
+    }
+
     ~scoped_resource()
     {
-        if(m_should_run)
+        if(m_execute_on_destruction)
         {
             m_deleter(m_resource);
         }
     }
-    //void invoke(invoke_it const strategy = invoke_it::once) noexcept;
-    //see below release() noexcept;
-    //void reset(R... newresource) noexcept;
-    //operator see below () const noexcept;
-    //template<size_t n = 0>
-    //see below get() const noexcept;
-    //see below operator*() const noexcept;
-    //see below operator->() const noexcept;
-    //const DELETER & get_deleter() const noexcept;
 
+    void invoke(const invoke_it strategy = std_opt::once) NOEXCEPT
+    {
+        if (execute_on_destruction)
+        {
+            m_deleter(m_resource);
+        }
+        execute_on_destruction = (strategy == invoke_it::again);
+    }
+
+    // Note: Difference with N3830, this does not return a tuple.
+    RES release() NOEXCEPT
+    {
+        m_execute_on_destruction = false;
+        return std::move(m_resource);
+    }
+
+    // Note: Difference with N3830, this does not take a tuple.
+    void reset(RES resource) NOEXCEPT
+    {
+        invoke(invoke_it::again);
+        m_resource = std::move(resource);
+    }
+
+    // Note: Difference with N3830, operator RES not implemented.
+
+    // Note: Difference with N3830, this does not return a tuple.
+    RES get() const NOEXCEPT
+    {
+        return m_resource;
+    }
+
+    // Note: Difference with N3830, operator* not implemented.
+    // Note: Difference with N3830, operator-> not implemented.
+
+    const DELETER& get_deleter() const NOEXCEPT
+    {
+        return m_deleter;
+    }
 };
 
 template<typename DELETER, typename RES>
