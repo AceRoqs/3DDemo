31d58f547bd71019520f1cc1e3bb73f88b991ce3
Toby Jones <ace_roqs@yahoo.com>
2014-03-27 01:49:29 -0700

    Fix reference.

--- PreCompile.h
+++ PreCompile.h
@@ -6,7 +6,7 @@
 // C++ Standard Library.
 #include <exception>
 #include <functional>
-#include "unique_resource.h"    // N3830 - Not yet in standard.
+#include "unique_resource.h"    // N3949 - Not yet in standard.
 
 // Windows API.
 #ifdef WIN32
