1b3e37f6d2a4abb9fb0d6be2b22718122ba5a097
Toby Jones <ace_roqs@yahoo.com>
2014-04-19 16:50:57 -0700

    Introduce tracing.

---
+++ Tracing.cpp
@@ -0,0 +1,25 @@
+#include "PreCompile.h"
+#include "Tracing.h"
+
+namespace WindowsCommon
+{
+
+void dprintf(_In_ PCSTR format, ...)
+{
+#ifndef NDEBUG
+    va_list args;
+    va_start(args, format);
+
+    char print_buffer[1024];
+    StringCbVPrintfA(print_buffer, sizeof(print_buffer), format, args);
+
+    va_end(args);
+
+    OutputDebugStringA(print_buffer);
+#else
+    UNREFERENCED_PARAMETER(format);
+#endif
+}
+
+}
+
---
+++ Tracing.h
@@ -0,0 +1,12 @@
+#ifndef TRACING_H
+#define TRACING_H
+
+namespace WindowsCommon
+{
+
+void dprintf(_In_ PCSTR format, ...);
+
+}
+
+#endif
+
