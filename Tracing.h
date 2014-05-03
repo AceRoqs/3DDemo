5f5b99630e0277746e27cd8aea8b8860366cc3ba
Toby Jones <ace_roqs@yahoo.com>
2014-05-02 20:32:40 -0700

    Add missing NOEXCEPT annotations to WindowsCommon.

--- Tracing.h
+++ Tracing.h
@@ -1,10 +1,12 @@
 #ifndef TRACING_H
 #define TRACING_H
 
+#include "PreCPP11.h"
+
 namespace WindowsCommon
 {
 
-void dprintf(_In_ PCSTR format, ...);
+void dprintf(_In_ PCSTR format, ...) NOEXCEPT;
 
 }
 
