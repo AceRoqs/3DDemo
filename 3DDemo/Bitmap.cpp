81a966b89fb80d66a9c71041020b20d6279fc130
Toby Jones <ace_roqs@yahoo.com>
2014-05-11 11:35:58 -0700

    Move Bitmap implementation out of header.

---
+++ 3DDemo/Bitmap.cpp
@@ -0,0 +1,38 @@
+#include "PreCompile.h"
+#include "Bitmap.h"
+
+Bitmap::Bitmap() :
+    xsize(0),
+    ysize(0),
+    filtered(false)
+{
+}
+
+// http://stackoverflow.com/questions/70013/how-to-detect-if-im-compiling-code-with-visual-studio-2008
+#if _MSC_VER == 1600 || _MSC_VER == 1700
+// Add default and move constructors as VS2010/2012 doesn't default define a move constructor.
+Bitmap::Bitmap(Bitmap&& other) :
+    bitmap(std::move(other.bitmap)),
+    xsize(other.xsize),
+    ysize(other.ysize),
+    filtered(other.filtered)
+{
+}
+
+Bitmap& Bitmap::operator=(Bitmap&& other) NOEXCEPT
+{
+    // Handle A=A case.
+    if(this != &other)
+    {
+        bitmap = std::move(other.bitmap);
+        xsize = other.xsize;
+        ysize = other.ysize;
+        filtered = other.filtered;
+    }
+
+    return *this;
+}
+#else
+#error This compiler may autodefine the default move constructor.
+#endif
+
--- 3DDemo/Bitmap.h
+++ 3DDemo/Bitmap.h
@@ -1,34 +1,15 @@
 #ifndef BITMAP_H
 #define BITMAP_H
 
-// TODO: Move implementations out of header.
 struct Bitmap
 {
-    Bitmap() : xsize(0), ysize(0), filtered(false) {}
+    Bitmap();
+
 // http://stackoverflow.com/questions/70013/how-to-detect-if-im-compiling-code-with-visual-studio-2008
 #if _MSC_VER == 1600 || _MSC_VER == 1700
     // Add default and move constructors as VS2010/2012 doesn't default define a move constructor.
-    Bitmap(Bitmap&& other) :
-        bitmap(std::move(other.bitmap)),
-        xsize(other.xsize),
-        ysize(other.ysize),
-        filtered(other.filtered)
-    {
-    }
-
-    Bitmap& operator=(Bitmap&& other) NOEXCEPT
-    {
-        // Handle A=A case.
-        if(this != &other)
-        {
-            bitmap = std::move(other.bitmap);
-            xsize = other.xsize;
-            ysize = other.ysize;
-            filtered = other.filtered;
-        }
-
-        return *this;
-    }
+    Bitmap(Bitmap&& other);
+    Bitmap& operator=(Bitmap&& other) NOEXCEPT;
 #else
 #error This compiler may autodefine the default move constructor.
 #endif
