6ab4afdbedf10cc4177b41a9d17c48ace38cdedc
Toby Jones <ace_roqs@yahoo.com>
2014-05-13 09:55:24 -0700

    Remove gltex.*.

--- 3DDemo/gltex.h
+++
@@ -1,7 +0,0 @@
-#ifndef GLTEX_H
-#define GLTEX_H
-
-void bind_bitmap_to_gl_texture(const struct Bitmap& bitmap, unsigned int texture_id);
-
-#endif
-
--- 3DDemo/render.cpp
+++ 3DDemo/render.cpp
@@ -4,7 +4,6 @@
 //=========================================================================
 #include "PreCompile.h"
 #include "render.h"
-#include "gltex.h"
 #include "particle.h"
 #include "world.h"
 #include "Bitmap.h"
@@ -25,6 +24,38 @@ static CEmitter emitter;
 
 static void BezCurve(GLfloat camera_x, GLfloat camera_y, GLfloat camera_z);
 
+static void bind_bitmap_to_gl_texture(const Bitmap& bitmap, unsigned int texture_id)
+{
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, texture_id);
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    if(bitmap.filtered)
+    {
+        // Bilinear filtering.
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    }
+    else
+    {
+        // Don't filter the texture.
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    }
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+    glTexImage2D(GL_TEXTURE_2D,
+                 0,
+                 3,
+                 bitmap.xsize,
+                 bitmap.ysize,
+                 0,
+                 GL_RGB,
+                 GL_UNSIGNED_BYTE,
+                 &bitmap.bitmap[0]);
+}
 
 void initialize_gl_constants()
 {
