98d68c45d1ba940f881a0eadf5f1bd44c5c1664b
Toby Jones <ace_roqs@yahoo.com>
2014-05-29 20:17:48 -0700

    Specify the final types by default.

--- 3DDemo/particle.cpp
+++ 3DDemo/particle.cpp
@@ -49,39 +49,39 @@ void CParticle::Draw(const Camera& camera, unsigned int texture_id) const
 {
     // transform to location
     glLoadIdentity();
-    glRotatef(camera.m_degrees, 0, 1, 0);
+    glRotatef(camera.m_degrees, 0.0f, 1.0f, 0.0f);
     glTranslatef(camera.m_position.x(), camera.m_position.y(), camera.m_position.z());
     glTranslatef(current_position.x(), current_position.y(), current_position.z());
 
     // billboard the sprite
-    glRotatef(-camera.m_degrees, 0, 1, 0);
+    glRotatef(-camera.m_degrees, 0.0f, 1.0f, 0.0f);
 
-//	glColor4f(color.red, color.green, color.blue, color.alpha);
-    glColor4f(1,0,0,1);
+//  glColor4f(color.red, color.green, color.blue, color.alpha);
+    glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
     glBindTexture(GL_TEXTURE_2D, texture_id);
-//	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-//	glBlendFunc(GL_ONE, GL_ZERO);
+//  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+//  glBlendFunc(GL_ONE, GL_ZERO);
     glBlendFunc(GL_ONE, GL_ONE);
     glBegin(GL_QUADS);
-//	glBegin(GL_LINE_LOOP);
+//  glBegin(GL_LINE_LOOP);
 /*
-        glTexCoord2f(0.0, 0.0);
-        glVertex3d(cur_x + 0.25, cur_y - 0.25, cur_z);
-        glTexCoord2f(1.0, 0.0);
-        glVertex3d(cur_x - 0.25, cur_y - 0.25, cur_z);
-        glTexCoord2f(1.0, 1.0);
-        glVertex3d(cur_x - 0.25, cur_y + 0.25, cur_z);
-        glTexCoord2f(0.0, 1.0);
-        glVertex3d(cur_x + 0.25, cur_y + 0.25, cur_z);
+        glTexCoord2f(0.0f, 0.0f);
+        glVertex3d(cur_x + 0.25f, cur_y - 0.25f, cur_z);
+        glTexCoord2f(1.0f, 0.0f);
+        glVertex3d(cur_x - 0.25f, cur_y - 0.25f, cur_z);
+        glTexCoord2f(1.0f, 1.0f);
+        glVertex3d(cur_x - 0.25f, cur_y + 0.25f, cur_z);
+        glTexCoord2f(0.0f, 1.0f);
+        glVertex3d(cur_x + 0.25f, cur_y + 0.25f, cur_z);
 */
-        glTexCoord2f(0.0, 0.0);
-        glVertex3d(0.25, -0.25, 0);
-        glTexCoord2f(1.0, 0.0);
-        glVertex3d(-0.25, -0.25, 0);
-        glTexCoord2f(1.0, 1.0);
-        glVertex3d(-0.25, 0.25, 0);
-        glTexCoord2f(0.0, 1.0);
-        glVertex3d(0.25, 0.25, 0);
+        glTexCoord2f(0.0f, 0.0f);
+        glVertex3d(0.25, -0.25, 0.0);
+        glTexCoord2f(1.0f, 0.0f);
+        glVertex3d(-0.25, -0.25, 0.0);
+        glTexCoord2f(1.0f, 1.0f);
+        glVertex3d(-0.25, 0.25, 0.0);
+        glTexCoord2f(0.0f, 1.0f);
+        glVertex3d(0.25, 0.25, 0.0);
     glEnd();
 }
 
@@ -98,15 +98,15 @@ void CEmitter::CreateParticle(unsigned int index)
     m_particles[index].current_velocity.x() = 0.027f - ((float)rand() / float(RAND_MAX)) / 18.0f;
     m_particles[index].current_velocity.y() = ((float)rand() / float(RAND_MAX)) / 10.0f;
     m_particles[index].current_velocity.z() = 0.027f - ((float)rand() / float(RAND_MAX)) / 18.0f;
-    m_particles[index].life = int(((float)rand() / float(RAND_MAX)) * 15.0);
-    m_particles[index].color.red = 1.0;
-    m_particles[index].color.green = 0.0;
-    m_particles[index].color.blue = 0.0;
-    m_particles[index].color.alpha = 1.0;
-    m_particles[index].final_color.red = 1.0;
-    m_particles[index].final_color.green = 0.0;
-    m_particles[index].final_color.blue = 0.0;
-    m_particles[index].final_color.alpha = 1.0;
+    m_particles[index].life = int(((float)rand() / float(RAND_MAX)) * 15.0f);
+    m_particles[index].color.red = 1.0f;
+    m_particles[index].color.green = 0.0f;
+    m_particles[index].color.blue = 0.0f;
+    m_particles[index].color.alpha = 1.0f;
+    m_particles[index].final_color.red = 1.0f;
+    m_particles[index].final_color.green = 0.0f;
+    m_particles[index].final_color.blue = 0.0f;
+    m_particles[index].final_color.alpha = 1.0f;
 }
 
 void CEmitter::Update(float elapsed_milliseconds)
@@ -124,9 +124,9 @@ void CEmitter::Update(float elapsed_milliseconds)
 void CEmitter::Draw(const Camera& camera, unsigned int texture_id) const
 {
 //    glLoadIdentity();
-//    glRotatef(camera_degrees, 0, 1, 0);
+//    glRotatef(camera_degrees, 0.0f, 1.0f, 0.0f);
 //    glTranslatef(camera_x, camera_y, camera_z);
-//    glRotatef(-camera_degrees, 0, 1, 0);
+//    glRotatef(-camera_degrees, 0.0f, 1.0f, 0.0f);
 /*
 float modelview[16];
     int i, j;
@@ -139,12 +139,13 @@ float modelview[16];
 
     // undo all rotations
     // beware all scaling is lost as well 
-    for( i=0; i<3; i++ ) 
-        for( j=0; j<3; j++ ) {
-        if ( i==j )
-            modelview[i*4+j] = 1.0;
-        else
-            modelview[i*4+j] = 0.0;
+    for(i = 0; i < 3; i++)
+        for(j = 0; j < 3; j++)
+        {
+            if (i == j)
+                modelview[i * 4 + j] = 1.0f;
+            else
+                modelview[i * 4 + j] = 0.0f;
         }
 
     // set the modelview with no rotations
