a176442c3a85bd5445d731f5734688a15ae345af
Toby Jones <ace_roqs@yahoo.com>
2014-05-30 18:36:23 -0700

    Extract vertex calculation. Make bezier_patch self-contained.

--- 3DDemo/render.cpp
+++ 3DDemo/render.cpp
@@ -10,12 +10,6 @@
 #include "Camera.h"
 #include "LinearAlgebra.h"
 
-const unsigned int quadratic_bezier_control_point_count = 3;
-struct bezier_patch
-{
-    unsigned int control_point_indices[quadratic_bezier_control_point_count * quadratic_bezier_control_point_count];
-};
-
 static const Vector3f bezier_control_points[] =
 {
     { -2, 0, -10 },     // 0
@@ -35,10 +29,17 @@ static const Vector3f bezier_control_points[] =
     { -4,-2, -10 },     // 14
 };
 
+const unsigned int quadratic_bezier_control_point_count = 3;
+struct bezier_patch
+{
+    const Vector3f* control_point_vertices;
+    unsigned int control_point_indices[quadratic_bezier_control_point_count * quadratic_bezier_control_point_count];
+};
+
 static const bezier_patch patches[] =
 {
-    { 0,  1,  2,  3,  4,  5,  6,  7,  8 },
-    { 2,  9, 10,  5, 11, 12,  8, 13, 14 },
+    { { bezier_control_points }, 0,  1,  2,  3,  4,  5,  6,  7,  8 },
+    { { bezier_control_points }, 2,  9, 10,  5, 11, 12,  8, 13, 14 },
 };
 
 // http://en.wikipedia.org/wiki/B%C3%A9zier_curve
@@ -66,45 +67,56 @@ static float bezier_quadratic_basis(unsigned int index, float t)
     return basis;
 }
 
+static Vector3f calculate_quadratic_bezier_vertex(const bezier_patch& patch, float t_u, float t_v)
+{
+    Vector3f vertex = make_vector(0.0f, 0.0f, 0.0f);
+
+    // Calculate the u,v point of the patch using three control points in each (u/v) direction.
+    for(unsigned int j = 0; j < quadratic_bezier_control_point_count; ++j)
+    {
+        const float basis_v = bezier_quadratic_basis(j, t_v);
+
+        for(unsigned int i = 0; i < quadratic_bezier_control_point_count; ++i)
+        {
+            const float basis = bezier_quadratic_basis(i, t_u) * basis_v;
+
+            // Q(u,v) = sum[i=0..2]sum[j=0..2] Bi(u)Bj(v)Pij
+            const size_t control_point_index = patch.control_point_indices[j * quadratic_bezier_control_point_count + i];
+            vertex += patch.control_point_vertices[control_point_index] * basis;
+        }
+    }
+
+    return vertex;
+}
+
 // TODO: 2014: It would make much more sense to do this in a compute shader to generate the data where they are used.
-static std::vector<Vector3f> generate_quadratic_bezier_quads(const Vector3f* control_points, const bezier_patch& patch, unsigned int patch_count)
+std::vector<Vector3f> generate_quadratic_bezier_quads(const bezier_patch& patch, unsigned int patch_count)
 {
     const auto curve_vertex_count = patch_count + 1;
 
-    // Q(u,v) = sum[i=0..2]sum[j=0..2] Bi(u)Bj(v)Pij
-    std::vector<Vector3f> vertices(curve_vertex_count * curve_vertex_count);
+    std::vector<Vector3f> vertices;
+    vertices.reserve(curve_vertex_count * curve_vertex_count);
 
     // Generate all of the points.
     for(unsigned int v = 0; v < curve_vertex_count; ++v)
     {
+        // Range [0..1].
+        const float t_v = v / static_cast<float>(patch_count);
+
         for(unsigned int u = 0; u < curve_vertex_count; ++u)
         {
-            Vector3f& vertex = vertices[v * curve_vertex_count + u];
-            vertex = make_vector(0.0f, 0.0f, 0.0f);
-
             // Range [0..1].
             const float t_u = u / static_cast<float>(patch_count);
-            const float t_v = v / static_cast<float>(patch_count);
 
-            // Calculate the u,v point of the patch using three control points in each (u/v) direction.
-            for(unsigned int j = 0; j < quadratic_bezier_control_point_count; ++j)
-            {
-                const float basis_v = bezier_quadratic_basis(j, t_v);
-
-                for(unsigned int i = 0; i < quadratic_bezier_control_point_count; ++i)
-                {
-                    const float basis = bezier_quadratic_basis(i, t_u) * basis_v;
-
-                    const size_t control_point_index = patch.control_point_indices[j * quadratic_bezier_control_point_count + i];
-                    vertex += control_points[control_point_index] * basis;
-                }
-            }
+            vertices.push_back(calculate_quadratic_bezier_vertex(patch, t_u, t_v));
         }
     }
 
     return vertices;
 }
 
+// Vertices is a two dimensional array of patch vertices.
+// TODO: 2014: This isn't very cache friendly, as the array isn't read in-order.  Vertex/Index arrays would also be an improvement.
 void draw_patch(const std::vector<Vector3f>& vertices, unsigned int patch_count, unsigned int texture_id)
 {
     const float scale = 1.0f / patch_count;
@@ -298,9 +310,9 @@ void draw_list(
     const unsigned int MAX_GENERATED_POINTS = 10;
     unsigned int patch_count = (unsigned int)(MAX_GENERATED_POINTS * 4 / (point_distance(camera.m_position, make_vector(2, 0, 10)))) - 1;
     patch_count = std::min(std::max(2u, patch_count), MAX_GENERATED_POINTS - 1);
-    std::vector<Vector3f> vertices = generate_quadratic_bezier_quads(bezier_control_points, patches[0], patch_count);
+    std::vector<Vector3f> vertices = generate_quadratic_bezier_quads(patches[0], patch_count);
     draw_patch(vertices, patch_count, 2);
-    vertices = generate_quadratic_bezier_quads(bezier_control_points, patches[1], patch_count);
+    vertices = generate_quadratic_bezier_quads(patches[1], patch_count);
     draw_patch(vertices, patch_count, 2);
 
 //    glUnlockArraysEXT();
