c281bcff85fa20db17792c83ff752df1569fa367
Toby Jones <ace_roqs@yahoo.com>
2014-08-21 17:38:45 -0700

    _MSC_VER corrections, particularly with respect to different MSVC compilers.

--- PreCPP11.h
+++ PreCPP11.h
@@ -4,24 +4,36 @@
 // Define C++11 constructs not available in pre-C++11 compilers.
 
 #ifdef _MSC_VER
-#if _MSC_VER != 1600
-#error Revisit the OVERRIDE, EQUALS_DELETE, and NOEXCEPT macros and use the C++11 version if the compiler supports them.
+#if _MSC_VER > 1800
+#error Revisit the NOEXCEPT macro and use the C++11 version if the compiler supports it.
 #endif
 #endif
 
 // "override" keyword.
 // http://en.wikipedia.org/wiki/C++11#Explicit_overrides_and_final
+#if defined(_MSC_VER) && _MSC_VER < 1700
 #define OVERRIDE
+#else
+#define OVERRIDE override
+#endif
 
 // = delete.
 // http://en.wikipedia.org/wiki/C++11#Explicitly_defaulted_and_deleted_special_member_functions
+#if defined(_MSC_VER) && _MSC_VER < 1800
 #define EQUALS_DELETE
+#else
+#define EQUALS_DELETE = delete
+#endif
 
-#if defined(_MSC_VER) && !defined(NDEBUG)
+#if defined(_MSC_VER)
+#if defined(NDEBUG)
+#define NOEXCEPT
+#else
 // MSVC + Debug build: use non-standard empty throw exception specification.
 #define NOEXCEPT throw()
+#endif
 #else
-#define NOEXCEPT
+#define NOEXCEPT noexcept
 #endif
 
 
