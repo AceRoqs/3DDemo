f1ae6abf90a536c9b67acb2ff253d0b34664d9bf
Toby Jones <ace_roqs@yahoo.com>
2014-12-22 23:16:58 -0800

    Add TODO.

--- PortableRuntime/CheckException.h
+++ PortableRuntime/CheckException.h
@@ -14,6 +14,7 @@ inline void check_exception(bool result)
 }
 
 // This macro should only be used to work around static analysis warnings.
+// TODO: passing a function as expr is not valid.  Check this.
 #define CHECK_EXCEPTION(expr) { PortableRuntime::check_exception(expr); __analysis_assume(expr); }
 
 }
