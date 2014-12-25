92736dcb3639ccdab5df02887ee212416b30cb1a
Toby Jones <ace_roqs@yahoo.com>
2014-12-24 20:50:30 -0800

    Fix static analysis error and style.

--- InputDevice.cpp
+++ InputDevice.cpp
@@ -5,11 +5,11 @@
 namespace WindowsCommon
 {
 
-Input_device::Input_device(_In_ HINSTANCE hInstance, _In_ HWND hwnd)
+Input_device::Input_device(_In_ HINSTANCE instance, _In_ HWND hwnd)
 {
     // Create DirectInput keyboard device.
     ATL::CComPtr<IDirectInput8> direct_input;
-    check_hr(DirectInput8Create(hInstance, DIRECTINPUT_VERSION, IID_IDirectInput8, reinterpret_cast<PVOID*>(&direct_input), nullptr));
+    check_hr(DirectInput8Create(instance, DIRECTINPUT_VERSION, IID_IDirectInput8, reinterpret_cast<PVOID*>(&direct_input), nullptr));
     check_hr(direct_input->CreateDevice(GUID_SysKeyboard, &m_device, nullptr));
 
     assert(c_dfDIKeyboard.dwDataSize == KEYBOARD_BUFFER_SIZE);
@@ -30,7 +30,6 @@ void Input_device::get_input(_Out_ Keyboard_state* keyboard_state) const
 {
     if(SUCCEEDED(m_device->Acquire()))
     {
-        assert(keyboard_state->size() <= MAXDWORD);
         check_hr(m_device->GetDeviceState(static_cast<DWORD>(keyboard_state->size()), keyboard_state));
     }
     else
