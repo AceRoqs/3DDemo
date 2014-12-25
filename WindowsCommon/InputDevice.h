b9e6a3b2b4aefb085cd4a46c32bd6fcab22e022a
Toby Jones <ace_roqs@yahoo.com>
2014-12-24 20:50:30 -0800

    Fix static analysis error and style.

--- WindowsCommon/InputDevice.h
+++ WindowsCommon/InputDevice.h
@@ -4,13 +4,14 @@
 namespace WindowsCommon
 {
 
+// Code assumes that KEYBOARD_BUFFER_SIZE <= MAXDWORD.
 #define KEYBOARD_BUFFER_SIZE 256
 typedef std::array<uint8_t, KEYBOARD_BUFFER_SIZE> Keyboard_state;
 
 class Input_device
 {
 public:
-    Input_device(_In_ HINSTANCE hInstance, _In_ HWND hwnd);
+    Input_device(_In_ HINSTANCE instance, _In_ HWND hwnd);
     ~Input_device();
     void get_input(_Out_ Keyboard_state* keyboard_state) const;
 
