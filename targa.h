caf993eb46488501a96e2de0d34f5b9eb8287bae
Toby Jones <ace_roqs@yahoo.com>
2015-01-05 15:30:03 -0800

    Types before prototypes, forward declarations for items in current namespace.

--- targa.h
+++ targa.h
@@ -1,12 +1,10 @@
 #pragma once
 
-#include "Bitmap.h"
-
 namespace ImageProcessing
 {
 
-Bitmap decode_bitmap_from_tga_memory(_In_reads_(size) const uint8_t* tga_memory, size_t size);
-std::vector<uint8_t> encode_tga_from_bitmap(const Bitmap& bitmap);
+struct Bitmap decode_bitmap_from_tga_memory(_In_reads_(size) const uint8_t* tga_memory, size_t size);
+std::vector<uint8_t> encode_tga_from_bitmap(const struct Bitmap& bitmap);
 
 }
 
