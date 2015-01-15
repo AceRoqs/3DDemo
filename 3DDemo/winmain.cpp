c64ad7b0751941499a8efc00ad83d6a267acd80b
Toby Jones <ace_roqs@yahoo.com>
2015-01-14 18:18:44 -0800

    Only trace during Debug builds.
    This likely doesn't exclude the tracing code in Release builds.  Think
    about how to mitigate this.

--- 3DDemo/winmain.cpp
+++ 3DDemo/winmain.cpp
@@ -16,7 +16,9 @@ int WINAPI _tWinMain(_In_ HINSTANCE instance,   // Handle to the program instanc
 {
     UNREFERENCED_PARAMETER(command_line);
 
+#ifndef NDEBUG
     PortableRuntime::set_dprintf(WindowsCommon::debugger_dprintf);
+#endif
 
     // Default the return code to 0, which is registered as ERRORLEVEL=0 in
     // a batch file.  This means running the app was successful.
