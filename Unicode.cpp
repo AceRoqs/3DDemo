ab50f26f95e3e463b6958012ca0a2e867aef24fb
Toby Jones <ace_roqs@yahoo.com>
2015-01-15 13:32:13 -0800

    Squashed 'PortableRuntime/' changes from c0fc3c1..f51a2fc
    f51a2fc Add untested UTF-8<-UTF-16.
    1116d81 Convert from UTF-16 not UCS-2.
    
    git-subtree-dir: PortableRuntime
    git-subtree-split: f51a2fc98035b68bf5d63a9fbc6706abb8f75a97

--- Unicode.cpp
+++ Unicode.cpp
@@ -8,13 +8,21 @@ namespace PortableRuntime
 std::wstring utf16_from_utf8(const std::string& utf8_string)
 {
     std::wstring utf16_string;
-    utf16_string = std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t>().from_bytes(utf8_string);
+    utf16_string = std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t>().from_bytes(utf8_string);
 
     return utf16_string;
 }
 
 // Untested, and not used anywhere.
 #if 0
+std::string utf8_from_utf16(const std::wstring& utf16_string)
+{
+    std::string utf8_string;
+    utf8_string = std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t>().to_bytes(utf16_string);
+
+    return utf8_string;
+}
+
 bool utf8_is_ascii(_In_z_ const char* utf8_string)
 {
     bool is_ascii = true;
--- Unicode.h
+++ Unicode.h
@@ -4,7 +4,6 @@ namespace PortableRuntime
 {
 
 std::wstring utf16_from_utf8(const std::string& utf8_string);
-//bool utf8_is_ascii(_In_z_ const char* utf8_string);
 
 }
 
