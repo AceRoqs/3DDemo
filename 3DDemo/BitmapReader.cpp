e95714cc8841f572a096d5b0651d261fb8ee4100
Toby Jones <ace_roqs@yahoo.com>
2016-10-09 23:49:20 -0700

    Add PixMap support.

--- 3DDemo/BitmapReader.cpp
+++ 3DDemo/BitmapReader.cpp
@@ -2,22 +2,13 @@
 #include "BitmapReader.h"       // Pick up forward declarations to ensure correctness.
 #include <ImageProcessing/pcx.h>
 #include <ImageProcessing/targa.h>
+#include <ImageProcessing/PixMap.h>
 #include <WindowsCommon/CheckHR.h>
 #include <WindowsCommon/Wrappers.h>
 #include <PortableRuntime/CheckException.h>
 
 namespace Demo
 {
-
-// This function is case sensitive due to the lack of library support for
-// UTF-8 case insensitive matching.
-// TODO: Consider case insensitive for ASCII subset of UTF-8.
-static bool file_has_extension_case_sensitive(_In_z_ const char* file_name, _In_z_ const char* extension) noexcept
-{
-    const size_t length_file = strlen(file_name);
-    const size_t length_extension = strlen(extension);
-    return ((length_file >= length_extension) && (strcmp(file_name + length_file - length_extension, extension) == 0));
-}
 
 // TODO: 2014: Previously, ImageProcessing had a dependency on WindowsCommon before this function was removed.
 // Figure out how to handle this type of glue code, and where code like this should live.
@@ -44,16 +35,20 @@ ImageProcessing::Bitmap bitmap_from_file(_In_z_ const char* file_name)
     }
     WaitForSingleObject(read_complete, INFINITE);
 
-    if(file_has_extension_case_sensitive(file_name, ".pcx"))
+    if(ImageProcessing::is_pcx_file_name(file_name))
     {
         return ImageProcessing::decode_bitmap_from_pcx_memory(buffer.data(), size);
     }
+    else if(ImageProcessing::is_tga_file_name(file_name))
+    {
+        return ImageProcessing::decode_bitmap_from_tga_memory(buffer.data(), size);
+    }
     else
     {
-        const bool is_targa = file_has_extension_case_sensitive(file_name, ".tga");
-        CHECK_EXCEPTION(is_targa, std::string("Unsupported file type: ") + file_name);
+        const bool is_ppm = ImageProcessing::is_pixmap_file_name(file_name);
+        CHECK_EXCEPTION(is_ppm, std::string("Unsupported file type: ") + file_name);
 
-        return ImageProcessing::decode_bitmap_from_tga_memory(buffer.data(), size);
+        return ImageProcessing::decode_bitmap_from_pixmap_memory(buffer.data(), size);
     }
 }
 
